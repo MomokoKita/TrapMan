29ffbbab0f33f1cfc454010ef956f1db5e5088f3
MOMOKAN_TABETAI <fumomosan@outlook.jp>
2020-12-05 16:12:13 +0900

    プロジェクト作成

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //プレイヤーの移動処理
+	public float speed = 3.0f;
+    private Vector3 Player_pos; //プレイヤーのポジション
+    private float x; //x方向のImputの値
+    private float z; //z方向のInputの値
+    private Rigidbody rigd;
+    //プレイヤーのアニメーション処理
+    private Animator animator = null;
+
+    void Start()
+    {
+        Player_pos = GetComponent<Transform>().position; //最初の時点でのプレイヤーのポジションを取得
+        rigd = GetComponent<Rigidbody>(); //プレイヤーのRigidbodyを取得
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("right") || Input.GetKey("left") || Input.GetKey("up") || Input.GetKey("down"))
+        {
+            animator.SetInteger("StateID",1);
+        }
+        else
+        {
+            animator.SetInteger("StateID", 0);
+        }
+        x = Input.GetAxis("Horizontal"); //x方向のInputの値を取得
+        z = Input.GetAxis("Vertical"); //z方向のInputの値を取得
+
+            //プレイヤーのRigidbodyに対してInputにspeedを掛けた値で更新し移動
+            rigd.velocity = new Vector3(x * speed, 0, z * speed);
+
+            //プレイヤーがどの方向に進んでいるかがわかるように、初期位置と現在地の座標差分を取得
+            Vector3 diff = transform.position - Player_pos;
+
+            //ベクトルの長さが0.01fより大きい場合にプレイヤーの向きを変える処理を入れる(0では入れないので）
+            if (diff.magnitude > 0.01f)
+            {
+                //ベクトルの情報をQuaternion.LookRotationに引き渡し回転量を取得しプレイヤーを回転させる
+                transform.rotation = Quaternion.LookRotation(diff);
+            }
+            Player_pos = transform.position; //プレイヤーの位置を更新
+
+    }
+
+}
---
+++ Assets/Scripts/Player_Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Camera : MonoBehaviour
+{
+    GameObject Player;
+    GameObject mainCamera;
+    // Use this for initialization
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        mainCamera = GameObject.Find("Main Camera");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        mainCamera.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, Player.transform.position.z - 10);
+    }
+}
