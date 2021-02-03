29812f8a44628263ca85af59934fb04319a898d1
NICOTYAN <fumomoSan@outlook.jp>
2021-02-03 11:55:12 +0900

    プッシュ二回目

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,20 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
-public class Player : MonoBehaviour
+public class Player : Character
 {
-    //プレイヤーの移動処理
-    public float speed = 3.0f;
-    private Vector3 Player_pos; //プレイヤーのポジション
-    private float x; //x方向のImputの値
-    private float z; //z方向のInputの値
-    private Rigidbody rigd;
-
-    //プレイヤーのアニメーション処理
-    private Animator animator = null;
-    private bool Carry_Flag = false;
-    private bool Carry_Flag_Idle = false;
-
     //足音のアニメーション
     [SerializeField] AudioClip[] clips;
     [SerializeField] float pitchRange = 0.1f;
@@ -24,21 +12,20 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-        Player_pos = GetComponent<Transform>().position; //最初の時点でのプレイヤーのポジションを取得
-        rigd = GetComponent<Rigidbody>(); //プレイヤーのRigidbodyを取得
-        animator = GetComponent<Animator>();
-
-
-        //足音
-        // アタッチしたオーディオソースのうち1番目を使用する
+        Init();
+        // (足音)アタッチしたオーディオソースのうち1番目を使用する
         source = GetComponents<AudioSource>()[0];
     }
 
     void Update()
     {
-        if (Input.GetKey("right") || Input.GetKey("left") || Input.GetKey("up") || Input.GetKey("down"))
+        Player_Camera();
+        if (Input.GetKey("t"))
         {
-            
+            TextRead();
+        }
+        if (Input.GetKey("d") || Input.GetKey("a") || Input.GetKey("w") || Input.GetKey("s"))
+        {           
             animator.SetInteger("StateID",1);
         }        
         else
@@ -46,51 +33,24 @@ public class Player : MonoBehaviour
             animator.SetInteger("StateID", 0);
         }
 
-        //モノを持つ判定
-        if(Input.GetKeyDown("space") && Carry_Flag == false)
-        {
-            Debug.Log("持った");
-            Carry_Flag = true;
-            Carry_Flag_Idle = true;
-            animator.SetInteger("StateID", 10);
-        }
-
-        else if (Input.GetKeyDown("space") && Carry_Flag == true)
-        {
-            Debug.Log("下した");
-            animator.SetInteger("StateID", 12);
-            Carry_Flag = false;
-            Carry_Flag_Idle = false;
-        }
-        else if (Carry_Flag_Idle)
-        {
-            animator.SetInteger("StateID", 11);
-            if (Input.GetKey("right") || Input.GetKey("left") || Input.GetKey("up") || Input.GetKey("down"))
-            {
-
-                animator.SetInteger("StateID", 14);
-            }
-        }
-
-
-        x = Input.GetAxis("Horizontal"); //x方向のInputの値を取得
-        z = Input.GetAxis("Vertical"); //z方向のInputの値を取得
-
-        //プレイヤーのRigidbodyに対してInputにspeedを掛けた値で更新し移動
-        rigd.velocity = new Vector3(x * speed, 0, z * speed);
-
-        //プレイヤーがどの方向に進んでいるかがわかるように、初期位置と現在地の座標差分を取得
-        Vector3 diff = transform.position - Player_pos;
-
-        //ベクトルの長さが0.01fより大きい場合にプレイヤーの向きを変える処理を入れる(0では入れないので）
-        if (diff.magnitude > 0.01f)
-        {
-            //ベクトルの情報をQuaternion.LookRotationに引き渡し回転量を取得しプレイヤーを回転させる
-            transform.rotation = Quaternion.LookRotation(diff);
-        }
-        Player_pos = transform.position; //プレイヤーの位置を更新
+        Carry();
+        Move();       
 
     }
+    void FixedUpdate()
+    {
+        // カメラの方向から、X-Z平面の単位ベクトルを取得
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+
+        // 方向キーの入力値とカメラの向きから、移動方向を決定
+        Vector3 moveForward = cameraForward * z + Camera.main.transform.right * x;
+
+        // 移動方向にスピードを掛ける。
+        rigd.velocity = moveForward * speed;
+
+    }
+
+
     public void PlayFootstepSE()
     {
         source.pitch = 1.0f + Random.Range(-pitchRange, pitchRange);
