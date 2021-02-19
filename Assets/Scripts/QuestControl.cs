38745efa731a743feb5afbafddd68ca5f67aa775
NICOTYAN <fumomoSan@outlook.jp>
2021-02-20 05:12:19 +0900

    完成（これ以上は限界なため仮眠します）

--- Assets/Scripts/QuestControl.cs
+++ Assets/Scripts/QuestControl.cs
@@ -1,26 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class QuestControl : SceneManager
 {
-    ////選択音
-    //[SerializeField] AudioClip[] clips;
-    //protected AudioSource source;
+    //選択音
+    [SerializeField] AudioClip[] clips;
+    protected AudioSource source;
 
-    [SerializeField] string Message;
+    void Start()
+    {
+        source = GetComponents<AudioSource>()[0];
+    }
 
     public void OnUserAction()
     {
         // オブジェクトからTextコンポーネントを取得
         Text score_text = uiText.GetComponent<Text>();
         // テキストの表示を入れ替える
-        score_text.text = Message;
         Invoke("ResetText", 2);
     }
-    public void OnClick_No()
+
+    public void GameClear()
     {
-        Debug.Log("No");
+        source.PlayOneShot(clips[0]);
+        Invoke("GameClearChange", 6);
+    }
+    public void GameOver()
+    {
+        source.PlayOneShot(clips[0]);
+        Invoke("GameOverChange", 6);
+    }
+
+    public void GameClearChange()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameClear");
+    }
+
+    public void GameOverChange()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
     }
 }
--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;  // 追加しましょう
 public class SceneManager : MonoBehaviour
 {
     public string TextTag = "Text";
-    public string QuestTag = "Quest";
+    public string QuestYesTag = "QuestYes";
+    public string QuestNoTag = "QuestNo";
 
     public GameObject uiText = null; // Textオブジェクト
 
@@ -22,9 +23,14 @@ public class SceneManager : MonoBehaviour
             //マウスクリックした場所からRayを飛ばし、オブジェクトがあればtrue 
             if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
             {
-                if (hit.collider.gameObject.CompareTag(QuestTag))
+                if (hit.collider.gameObject.CompareTag(QuestYesTag))
                 {
-                    hit.collider.gameObject.GetComponent<QuestControl>().OnUserAction();
+                    hit.collider.gameObject.GetComponent<QuestControl>().GameClear();
+                }
+
+                if (hit.collider.gameObject.CompareTag(QuestNoTag))
+                {
+                    hit.collider.gameObject.GetComponent<QuestControl>().GameOver();
                 }
 
                 if (hit.collider.gameObject.CompareTag(TextTag))
